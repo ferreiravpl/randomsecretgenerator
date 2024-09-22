f5ac6df1e735724f1c36bcb987dc439e4a8f8cca
Lucas Ferreira <ferreiravpl@gmail.com>
2024-09-22 15:58:02 -0300

    Ajuste do script para diminuir redundância de funções chamadas

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
  //Variáveis para serem utilizadas no script;
-let inputSliderValue = document.getElementById("inputPasswordSizeValue");
-let inputSlider = document.getElementById("inputPasswordSize");
+let inputPasswordSpan = document.getElementById("inputPasswordSizeValue");
+let inputPasswordRange = document.getElementById("inputPasswordSize");
 let inputPassword = document.getElementById("inputPassword");
 let lowercaseCheckbox = document.getElementById("lowerCaseLetters");
 let uppercaseCheckbox = document.getElementById("upperCaseLetters");
@@ -12,13 +12,14 @@ let lowerCaseLetters = "abcdefghijklmnopqrstuvwxyz";
 let upperCaseLetters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
 let numbers = "0123456789";
 let specialCharacters = "~!@#$%^&*"
+let possiblePasswordChars = "";
 
 //Exibe o valor default do input range;
-inputSliderValue.textContent = inputSlider.value;
+inputPasswordSpan.textContent = inputPasswordRange.value;
 
 //EventListener para definir novo tamanho da senha no input range (caso haja alteração);
-inputSlider.addEventListener('input', ()=>{
-    inputSliderValue.textContent = inputSlider.value;
+inputPasswordRange.addEventListener('input', ()=>{
+    inputPasswordSpan.textContent = inputPasswordRange.value;
 });
 
 //EventListener para o botão de gerar senha chamar a função generatePassword e exibir o valor no "inputPassword" do HTML;
@@ -29,10 +30,9 @@ generatePasswordButton.addEventListener('click', ()=>{
 //Função para gerar a senha
 function generatePassword(){
 
-    //Cria as variáveis para armazenar a senha que será gerada e os possíveis caracteres da senha, além de seu tamanho;
+    //Cria as variáveis para armazenar a senha que será gerada, além de seu tamanho;
     let generatedPassword = "";
-    let possiblePasswordChars = "";
-    let passwordSizeChosenByUser = inputSlider.value;
+    let passwordSizeChosenByUser = inputPasswordRange.value;
   
     //Checa se existem checkboxes marcados e suas quantidades para seguir o fluxo;
     if (!validateCheckboxes(passwordSizeChosenByUser)) {
@@ -40,22 +40,17 @@ function generatePassword(){
     }
 
     //Inicia o preenchimento da senha com caracteres do checkbox;
-    generatedPassword = fillGeneratedPassword(generatedPassword);
+    generatedPassword = fillWithCheckboxesChars(generatedPassword);
 
     //Preenche o resto da senha com caracteres aleatórios;
     for (let i = generatedPassword.length; i < passwordSizeChosenByUser; i++) {
-        possiblePasswordChars += lowercaseCheckbox.checked ? lowerCaseLetters : "";
-        possiblePasswordChars += uppercaseCheckbox.checked ? upperCaseLetters : "";
-        possiblePasswordChars += numbersCheckbox.checked ? numbers : "";
-        possiblePasswordChars += specialCharactersCheckbox.checked ? specialCharacters : "";
-
         generatedPassword += possiblePasswordChars.charAt(Math.floor(Math.random() * possiblePasswordChars.length));
     }
     //Retorna a senha final gerada;
     return generatedPassword;
 }
 
-//Função para validar os checkboxes marcados;
+//Função para validar os checkboxes marcados e se estão de acordo com o tamanho da senha;
 function validateCheckboxes (passwordSizeChosenByUser) {
     let totalCheckParameters = document.querySelectorAll('input[name=checkParameter]:checked').length;
 
@@ -73,29 +68,33 @@ function validateCheckboxes (passwordSizeChosenByUser) {
 }
 
 //Função para iniciar o preenchimento da senha com caracteres do checkbox (garante que tenha ao menos um tipo de caracter de cada checkbox marcado);
-function fillGeneratedPassword(generatedPassword) {
+function fillWithCheckboxesChars(generatedPassword) {
     if (lowercaseCheckbox.checked) {
-        generatedPassword += getRandomCharacterPerCheckbox(lowerCaseLetters);
+        generatedPassword += getRandomCharacter(lowerCaseLetters);
+        possiblePasswordChars += lowerCaseLetters;
     }
 
     if (uppercaseCheckbox.checked) {
-        generatedPassword += getRandomCharacterPerCheckbox(upperCaseLetters);
+        generatedPassword += getRandomCharacter(upperCaseLetters);
+        possiblePasswordChars += upperCaseLetters;
     }
 
     if (numbersCheckbox.checked) {
-        generatedPassword += getRandomCharacterPerCheckbox(numbers);
+        generatedPassword += getRandomCharacter(numbers);
+        possiblePasswordChars += numbers;
     }
 
     if (specialCharactersCheckbox.checked) {
-        generatedPassword += getRandomCharacterPerCheckbox(specialCharacters);
+        generatedPassword += getRandomCharacter(specialCharacters);
+        possiblePasswordChars += specialCharacters;
     }
 
     return generatedPassword;
 }
 
 //Função para buscar um caracter aleatório de cada checkbox;
-function getRandomCharacterPerCheckbox(checkboxCharacters) {
-    return checkboxCharacters.charAt(Math.floor(Math.random() * checkboxCharacters.length));
+function getRandomCharacter(charsFromCheckedCheckbox) {
+    return charsFromCheckedCheckbox.charAt(Math.floor(Math.random() * charsFromCheckedCheckbox.length));
 }
 
 //EventListener para copiar a senha
